d20fb3f56a33fb65c45c828a60f5f9f81ef8aff2
Itachi <samcravatta@gmail.com>
2019-12-06 12:27:26 -0600

    Initial Commit

---
+++ Program 1-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Program 1-1
+//
+//  Created by Sam Cravatta on 12/6/19.
+//  Copyright © 2019 Leaf_Programming. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
